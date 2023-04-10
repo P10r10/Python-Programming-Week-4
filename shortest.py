5319e4348da28ffe7d5c3757b2de65b03386542e
P10R10 <asantiago@keemail.me>
2023-04-10 09:31:03 +0100

    shortest

--- shortest.py
+++ shortest.py
@@ -1,13 +1,3 @@
-# def shortest(lst: list[str]):
-#     short = len(lst[0])
-#     idx = 0
-#     idx_short = 0
-#     while idx < len(lst):
-#         if len(lst[idx]) < short:
-#             idx_short = idx
-#             short = len(lst[idx])
-#         idx += 1
-#     return lst[idx_short]
 def shortest(names: list):
     return min(names, key=len)
 
