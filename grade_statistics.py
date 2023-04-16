b90e8552a1c54b254313fa711923f5537e30d02e
P10R10 <asantiago@keemail.me>
2023-04-16 15:59:55 +0100

    grade_statistics v2

--- grade_statistics.py
+++ grade_statistics.py
@@ -10,18 +10,10 @@ def get_results():
 
 
 def get_grade(points):
-    if points <= 14:
-        return 0
-    if points <= 17:
-        return 1
-    if points <= 20:
-        return 2
-    if points <= 23:
-        return 3
-    if points <= 27:
-        return 4
-    if points <= 30:
-        return 5
+    threshold = [14, 17, 20, 23, 27, 30]
+    for i in range(6):
+        if points <= threshold[i]:
+            return i
 
 
 def get_total_points(results: list):
@@ -31,10 +23,8 @@ def get_total_points(results: list):
 def get_final_grades(results: list):
     final_grades = []
     for exam, exercise in results:
-        if exam < 10:
-            final_grades.append(0)
-        else:
-            final_grades.append(get_grade(exam + int(exercise * 0.1)))
+        final_grades.append(0) if exam < 10 \
+            else final_grades.append(get_grade(exam + int(exercise * 0.1)))
     return final_grades
 
 
@@ -43,12 +33,9 @@ def print_statistics(points: list):
     print("Points average:", sum(get_total_points(points)) / len(points))
     pp = (len(points) - get_final_grades(points).count(0)) * 100 / len(points)
     print(f"Pass percentage: {pp:.1f}")
-    print(f"""Grade distribution:
-  5: {"*" * get_final_grades(points).count(5)}
-  4: {"*" * get_final_grades(points).count(4)}
-  3: {"*" * get_final_grades(points).count(3)}
-  2: {"*" * get_final_grades(points).count(2)}
-  1: {"*" * get_final_grades(points).count(1)}
-  0: {"*" * get_final_grades(points).count(0)}""")
+    print("Grade distribution:")
+    for n in range(5, -1, -1):
+        print(f"{n}:", '*' * get_final_grades(points).count(n))
+
 
 print_statistics(get_results())
